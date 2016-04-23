cbf4c3f029f98e7d401d4f15804d0455dd29207e
yyhhrr <yyhhrr@hotmail.com>
2016-04-23 15:20:58 +0800

    im ai

--- src/game/playerbot/PlayerbotAI.cpp
+++ src/game/playerbot/PlayerbotAI.cpp
@@ -3792,7 +3792,7 @@ void PlayerbotAI::UpdateAI(const uint32 /*p_time*/)
 			// resurrect now
 			// DEBUG_LOG ("[PlayerbotAI]: UpdateAI - Reviving %s to corpse...", m_bot->GetName() );
 
-			SetIgnoreUpdateTime(6);
+			SetIgnoreUpdateTime(2);
 
 			PlayerbotChatHandler ch(GetMaster());
 			if (!ch.revive(*m_bot))
@@ -3817,7 +3817,7 @@ void PlayerbotAI::UpdateAI(const uint32 /*p_time*/)
 		// set state to dead
 		SetState(BOTSTATE_DEAD);
 		// wait 30sec
-		SetIgnoreUpdateTime(30);
+		SetIgnoreUpdateTime(2);
 
 		return;
 	}
@@ -5491,7 +5491,7 @@ bool PlayerbotAI::TradeCopper(uint32 copper)
 
 bool PlayerbotAI::DoTeleport(WorldObject& /*obj*/)
 {
-	SetIgnoreUpdateTime(6);
+	SetIgnoreUpdateTime(2);
 	PlayerbotChatHandler ch(GetMaster());
 	if (!ch.teleport(*m_bot))
 	{
@@ -5504,7 +5504,7 @@ bool PlayerbotAI::DoTeleport(WorldObject& /*obj*/)
 
 void PlayerbotAI::HandleTeleportAck()
 {
-	SetIgnoreUpdateTime(6);
+	SetIgnoreUpdateTime(2);
 	m_bot->GetMotionMaster()->Clear(true);
 	if (m_bot->IsBeingTeleportedNear())
 	{
