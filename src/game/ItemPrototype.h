dbf9a0e960392fff807d17dd8d8b1269cb91ea35
tedvals <tedvals@hotmail.com>
2016-12-08 19:42:37 +0200

    Backport patch13

--- src/game/ItemPrototype.h
+++ src/game/ItemPrototype.h
@@ -273,7 +273,7 @@ enum ItemSubclassTradeGoods
     ITEM_SUBCLASS_PARTS                         = 1,
     ITEM_SUBCLASS_EXPLOSIVES                    = 2,
     ITEM_SUBCLASS_DEVICES                       = 3,
-    // ITEM_SUBCLASS_JEWELCRAFTING                 = 4,
+    ITEM_SUBCLASS_JEWELCRAFTING                 = 4,
     ITEM_SUBCLASS_CLOTH                         = 5,
     ITEM_SUBCLASS_LEATHER                       = 6,
     ITEM_SUBCLASS_METAL_STONE                   = 7,
